21d19664181338c9a384ad83799eb866054338b3
MD Eftekharul Islam <75820464+Eftekharul21@users.noreply.github.com>
2020-12-13 17:48:04 +0600

    Add files via upload

---
+++ Compare Two Integer Number.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int numl=5,num2=6;
+
+    if(numl==num2)
+        {
+            printf("Numbers are equal.");
+        }
+    else
+        {
+            printf("Numbers are not equal.");
+        }
+        return 0;
+}
